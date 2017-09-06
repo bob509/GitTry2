770e50dc3e6fdcc6b7ca3db1aac867b003db2ffc
robert schneider <robert.schneider@lehman.cuny.edu>
2017-09-06 10:19:11 -0400

    We compare our methods to normal linear regression. We also allow more values in second coordinate. The student should try different cost functions to see how they compare to normal regression and what graph looks like. Explain any problems theoretically.

--- RegressionClassGit.py
+++ RegressionClassGit.py
@@ -8,31 +8,31 @@ from pylab import meshgrid
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
-
+from scipy import stats
 
 rng = np.random      
-N=2 # number of different points for regression; I am only allowing values of -1 and 1
+N=5 # number of different points for regression; I am only allowing values of -1 and 1
 Xarray=10*rng.randn(N)
 print("Xvalues ",Xarray,"\nshape of Xarray ",Xarray.shape)
-
-V= rng.randint(size=N, low=0, high=2)
+Vlow=-10;Vhigh=10 #originally 0 and 2 to mimic perceptron 
+V= rng.randint(size=N, low=Vlow, high=Vhigh)
 
 V=2*V-1  #note that the constant 2 is broadcast through the array as it should in linear algebra
-print("values taken on ",V, "\nshape of V \n",V.shape)
+print("values taken on by V",V, "\nshape of V \n",V.shape)
 
 if N==2 and Xarray[1]!= Xarray[0] :
     wans= (V[1]-V[0])/(Xarray[1]-Xarray[0])
     bans= V[0]-wans*Xarray[0]
-    
+slope, intercept, r_value, p_value, std_err = stats.linregress(Xarray,V)    
     
 def cost(w,b):
     costArray=((w*Xarray+b)-V)**2 # taking square here to eventually take sum of squares
     return costArray.sum()
-print ("cost = \n",cost(1,1))
+#print ("cost = \n",cost(1,1))
 
 def costWrapper(Avar):# just a wrapper function for sciPy
     return cost(Avar[0],Avar[1]) # I think I can give it an ordered pair
-print("the cost at (1,1)",cost(-2,1))
+
 
 #
 #vcostWrapper= np.vectorize(costWrapper)  #did not work wanted to be able to work on array of pairs
@@ -42,11 +42,11 @@ print("the cost at (1,1)",cost(-2,1))
 #find the minimum from sciPy ; sort of ridiculous for square as is normal regression you can solve analytically
 res = minimize(costWrapper,[.5,.5],method='Nelder-Mead')# this method doesnt use derivatives
 print("result is ",res)
-print(" the actual minimimum values of w, b  are ",res.x[0],res.x[1])
+print(" the  results from our minimizer of values of w, b  are ",res.x[0],res.x[1])
 print("the minimum it reached is ",res.fun)
 if N== 2 and Xarray[1]!= Xarray[0] :
     print("the actual answer which should give us 0 as a cost is \n ",wans,bans)
-
+print("\n the answer from regression slope, intercept,error is \n",slope,intercept,cost(slope,intercept))
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 # !!!!!!!!!!!!!!!!! should change -.8 to 1 to really see more correct graph !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
